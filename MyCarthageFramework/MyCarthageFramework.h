5e7170a59376d5fd21a6193e7d0106134a5841c1
yong <1029891106@qq.com>
2017-03-19 23:56:48 +0800

    add gitignore

---
+++ MyCarthageFramework/MyCarthageFramework.h
@@ -0,0 +1,19 @@
+//
+//  MyCarthageFramework.h
+//  MyCarthageFramework
+//
+//  Created by wangyong on 2017/3/19.
+//  Copyright © 2017年 wujiadong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyCarthageFramework.
+FOUNDATION_EXPORT double MyCarthageFrameworkVersionNumber;
+
+//! Project version string for MyCarthageFramework.
+FOUNDATION_EXPORT const unsigned char MyCarthageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCarthageFramework/PublicHeader.h>
+
+
